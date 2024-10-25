7b6914b4131fdf5e0b2623a2862861733baeb3b0
minjeoong <orange_mj@naver.com>
2024-10-26 02:20:01 +0900

    feat: members 정보 저장 및 로드 기능구현

---
+++ week2/과제/script/memberLoad.js
@@ -0,0 +1,37 @@
+
+/*
+  @description
+    - membersData를 localStorage에 저장
+    - membersData를 가져와서 테이블에 출력
+ */
+
+import {members} from "../config/members.js";
+
+if (!localStorage.getItem("membersData")) {
+  localStorage.setItem("membersData", JSON.stringify(members));
+}
+
+
+const displayMembers = () => {
+  const membersData = JSON.parse(localStorage.getItem("membersData"));
+  const tbody = document.querySelector("table tbody");
+  tbody.innerHTML = "";
+
+  membersData.forEach((member) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${member.id}</td>
+      <td>${member.name}</td>
+      <td>${member.englishName}</td>
+      <td>${member.github}</td>
+      <td>${member.gender}</td>
+      <td>${member.role}</td>
+      <td>${member.firstWeekGroup}</td>
+      <td>${member.secondWeekGroup}</td>
+    `;
+    tbody.appendChild(row);
+  });
+
+}
+
+document.addEventListener("DOMContentLoaded", displayMembers);
