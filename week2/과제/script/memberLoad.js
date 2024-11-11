cc7383bd88e1dcdfc76d0dd275c20979a0e16179
minjeoong <orange_mj@naver.com>
2024-11-12 02:38:35 +0900

    feat: 드롭다운 초기화 적용

--- week2/과제/script/memberLoad.js
+++ week2/과제/script/memberLoad.js
@@ -1,4 +1,4 @@
-import {members} from "../config/members.js";
+import { members } from "../config/members.js";
 
 if (!localStorage.getItem("membersData")) {
   localStorage.setItem("membersData", JSON.stringify(members));
@@ -19,20 +19,18 @@ const displayMembers = (membersData) => {
       <td>${member.name}</td>
       <td>${member.englishName}</td>
       <td><a href="https://github.com/${member.github}">${member.github}</a></td>
-      <td>${member.gender === "male" ? "남성": "여성"}</td>
+      <td>${member.gender === "male" ? "남성" : "여성"}</td>
       <td>${member.role}</td>
       <td>${member.firstWeekGroup}</td>
       <td>${member.secondWeekGroup}</td>
     `;
     tbody.appendChild(row);
   });
-
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   displayMembers(JSON.parse(localStorage.getItem("membersData")));
 });
-
 
 /*
   @description
@@ -43,32 +41,36 @@ const searchButton = document.querySelector(".search-button");
 
 const filterMembers = () => {
   const nameFilter = document.getElementById("name").value;
-  const englishNameFilter = document.getElementById("english-name").value.toLowerCase();
+  const englishNameFilter = document
+    .getElementById("english-name")
+    .value.toLowerCase();
   const githubFilter = document.getElementById("github").value.toLowerCase();
   const genderFilter = document.getElementById("gender").value;
   const roleFilter = document.getElementById("role").value;
   const firstWeekGroupFilter = document.getElementById("firstWeekGroup").value;
-  const secondWeekGroupFilter = document.getElementById("secondWeekGroup").value;
+  const secondWeekGroupFilter =
+    document.getElementById("secondWeekGroup").value;
 
   const membersData = JSON.parse(localStorage.getItem("membersData"));
 
-  const filteredData = membersData.filter(member => {
+  const filteredData = membersData.filter((member) => {
     return (
       (!nameFilter || member.name.includes(nameFilter)) &&
-      (!englishNameFilter || member.englishName.toLowerCase().includes(englishNameFilter)) &&
+      (!englishNameFilter ||
+        member.englishName.toLowerCase().includes(englishNameFilter)) &&
       (!githubFilter || member.github.includes(githubFilter)) &&
-      ( genderFilter === "all" || member.gender === genderFilter) &&
-      ( roleFilter ==="all" || member.role === roleFilter) &&
-      (!firstWeekGroupFilter || member.firstWeekGroup.toString() === firstWeekGroupFilter) &&
-      (!secondWeekGroupFilter || member.secondWeekGroup.toString() === secondWeekGroupFilter)
+      (genderFilter === "all" || member.gender === genderFilter) &&
+      (roleFilter === "all" || member.role === roleFilter) &&
+      (!firstWeekGroupFilter ||
+        member.firstWeekGroup.toString() === firstWeekGroupFilter) &&
+      (!secondWeekGroupFilter ||
+        member.secondWeekGroup.toString() === secondWeekGroupFilter)
     );
   });
   displayMembers(filteredData);
 };
 
 searchButton.addEventListener("click", filterMembers);
-
-
 
 /*
   @description
@@ -81,16 +83,15 @@ const resetFilter = () => {
   document.getElementById("name").value = "";
   document.getElementById("english-name").value = "";
   document.getElementById("github").value = "";
-  document.getElementById("gender").value = "";
-  document.getElementById("role").value = "";
+  document.getElementById("gender").value = "all";
+  document.getElementById("role").value = "all";
   document.getElementById("firstWeekGroup").value = "";
   document.getElementById("secondWeekGroup").value = "";
 
   displayMembers(JSON.parse(localStorage.getItem("membersData")));
-}
+};
 
 resetButton.addEventListener("click", resetFilter);
-
 
 /*
   @description
@@ -104,21 +105,23 @@ const checkAll = (e) => {
   memberCheckboxes.forEach((checkbox) => {
     checkbox.checked = e.target.checked;
   });
-}
+};
 
 const updateSelectAll = () => {
   const memberCheckboxes = document.querySelectorAll(".member-checkbox"); // 동적으로 가져오기
   // noteList 를 배열로 변환, every 로 배열의 모든 요소가 true일 때만 true 반환
-  const allChecked = Array.from(memberCheckboxes).every((checkbox) => checkbox.checked);
-  const anyChecked = Array.from(memberCheckboxes).some((checkbox) => checkbox.checked);
+  const allChecked = Array.from(memberCheckboxes).every(
+    (checkbox) => checkbox.checked,
+  );
+  const anyChecked = Array.from(memberCheckboxes).some(
+    (checkbox) => checkbox.checked,
+  );
 
   // 모든 체크박스가 체크되면 전체 선택 활성화, 모두 체크 해제되면 비활성화
   selectAll.checked = allChecked;
   // 일부만 체크된 경우 중간 상태
   selectAll.indeterminate = !allChecked && anyChecked;
-
-}
-
+};
 
 selectAll.addEventListener("click", checkAll);
 document.addEventListener("click", (event) => {
@@ -126,7 +129,6 @@ document.addEventListener("click", (event) => {
     updateSelectAll();
   }
 });
-
 
 /*
   @description
@@ -139,15 +141,16 @@ const deleteMembers = () => {
   let membersData = JSON.parse(localStorage.getItem("membersData"));
   const checkedMembers = document.querySelectorAll(".member-checkbox:checked");
   const deleteMemId = Array.from(checkedMembers).map((member) => {
-    return member.dataset.id
+    return member.dataset.id;
   });
-  membersData = membersData.filter(member => !deleteMemId.includes(member.id.toString()));
+  membersData = membersData.filter(
+    (member) => !deleteMemId.includes(member.id.toString()),
+  );
   localStorage.setItem("membersData", JSON.stringify(membersData));
   displayMembers(membersData);
-}
+};
 
 deleteButton.addEventListener("click", deleteMembers);
-
 
 /*
   @description
@@ -161,11 +164,11 @@ const addSubmit = document.querySelector(".add-submit");
 
 const onModalOpen = () => {
   modal.style.display = "block";
-}
+};
 
 const onModalClose = () => {
   modal.style.display = "none";
-}
+};
 
 const onModalAddItem = () => {
   const name = document.getElementById("add_name").value;
@@ -177,7 +180,15 @@ const onModalAddItem = () => {
   const secondWeekGroup = document.getElementById("add_secondWeekGroup").value;
   const membersData = JSON.parse(localStorage.getItem("membersData")) || [];
 
-  if (!name || !englishName || !github || !gender || !role || !firstWeekGroup || !secondWeekGroup) {
+  if (
+    !name ||
+    !englishName ||
+    !github ||
+    !gender ||
+    !role ||
+    !firstWeekGroup ||
+    !secondWeekGroup
+  ) {
     alert("모든 항목을 입력해주세요.");
     return;
   }
@@ -194,7 +205,7 @@ const onModalAddItem = () => {
   localStorage.setItem("membersData", JSON.stringify(membersData));
   displayMembers(membersData);
   onModalClose();
-}
+};
 
 openModal.addEventListener("click", onModalOpen);
 closeModal.addEventListener("click", onModalClose);
